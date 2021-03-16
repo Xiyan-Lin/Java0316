ecadfb2efdba002ccf2faa556b832bcc365b91cc
Xiyan-Lin <80758569+Xiyan-Lin@users.noreply.github.com>
2021-03-16 20:35:13 +0800

    Update HelloWorld.java

--- src/main/java/com/ocp/day01/HelloWorld.java
+++ src/main/java/com/ocp/day01/HelloWorld.java
@@ -4,6 +4,7 @@ package com.ocp.day01;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello Java的第一堂課 ");
+        System.out.println("Hello Java的第一堂課 ");
     }
           
 }
