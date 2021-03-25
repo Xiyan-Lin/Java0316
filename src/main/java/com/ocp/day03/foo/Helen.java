fcfdef1122a66a3a28ebcd6586e805f55a15524c
MB-study <MB-study@192.168.1.33>
2021-03-25 21:58:51 +0800

    day03

---
+++ src/main/java/com/ocp/day03/foo/Helen.java
@@ -0,0 +1,13 @@
+package com.ocp.day03.foo;
+
+import com.ocp.day03.bar.John;
+public class Helen  extends John {
+    public void print() {
+      System.out.print("Helen: ");
+      System.out.print(a + ", ");
+      System.out.print(b + ", ");
+      // System.out.print(c + ", ");
+      // System.out.print(d + "\n");
+    }
+    
+}
