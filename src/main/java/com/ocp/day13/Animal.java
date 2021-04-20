a531989ba55f2367b0a27891a2285a8289b26929
MB-study <MB-study@192.168.1.33>
2021-04-20 21:42:35 +0800

    JAVA 0316

---
+++ src/main/java/com/ocp/day13/Animal.java
@@ -0,0 +1,9 @@
+
+package com.ocp.day13;
+
+
+public interface Animal {
+    public static final int legs = 4;
+    void eat();
+    void move(); 
+}
---
+++ src/main/java/com/ocp/day13/EPet.java
@@ -0,0 +1,17 @@
+
+package com.ocp.day13;
+
+
+public class EPet extends Pet {
+
+    @Override
+    public void move() {
+        System.out.println("電");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("震動");
+    }
+    
+}
